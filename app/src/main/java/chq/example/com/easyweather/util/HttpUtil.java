dd9f24541f9ec9f3794493bfd8e812203e5d991f
chquanquan <380341629@qq.com>
2017-05-11 14:30:34 +0800

    创建了选择省市区的代码

---
+++ app/src/main/java/chq/example/com/easyweather/util/HttpUtil.java
@@ -0,0 +1,18 @@
+package chq.example.com.easyweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by quan on 2017/5/11.
+ */
+
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+
+}
